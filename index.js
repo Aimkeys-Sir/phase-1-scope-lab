c6de985914a34cc32d7ec82daa85d5da17fc2b5d
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-10 09:01:56 +0300

    commit tree c43a1957431f0e8e47629803f2b63d2e7bad328c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName= "bob"
+const leastFavoriteCustomer="you"
+
+
+function upperCaseCustomerName()
+{
+    customerName= customerName.toUpperCase()
+}
+
+function setBestCustomer()
+{
+    bestCustomer="not bob"
+    return bestCustomer
+}
+function overwriteBestCustomer()
+{
+    bestCustomer="maybe bob"
+    return bestCustomer
+}
+function changeLeastFavoriteCustomer()
+{
+    leastFavoriteCustomer="not you"
+    return leastFavoriteCustomer
+}
+ //console.log(upperCaseCustomerName())
